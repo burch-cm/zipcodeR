a4093bd151d599869e6eb5b8095f312abba7bbe1
Kitt Burch <christopher.m.burch@gmail.com>
2022-03-07 08:52:54 -0500

    paired swaps
    swaps are pairs between office and field

--- R/download_data.r
+++ R/download_data.r
@@ -79,6 +79,29 @@ download_zip_data <- function(force = FALSE) {
   # Read in the new data
   zip_code_db <- dbGetQuery(conn, "SELECT * FROM simple_zipcode")
 
+  ##### insert missing zip code lat/lng #####
+  zip_code_db[zip_code_db$zipcode == "20591", "lat"] <- 38.88754600
+  zip_code_db[zip_code_db$zipcode == "20591", "lng"] <- -77.02234500
+
+  zip_code_db[zip_code_db$zipcode == "08405", "lat"] <- 39.35994800
+  zip_code_db[zip_code_db$zipcode == "08405", "lng"] <- -74.43353700
+
+  zip_code_db[zip_code_db$zipcode == "15231", "lat"] <- 40.49276700
+  zip_code_db[zip_code_db$zipcode == "15231", "lng"] <- -80.24290100
+
+  zip_code_db[zip_code_db$zipcode == "60666", "lat"] <- 41.97896400
+  zip_code_db[zip_code_db$zipcode == "60666", "lng"] <- -87.90241700
+
+  zip_code_db[zip_code_db$zipcode == "63145", "lat"] <- 38.74729100
+  zip_code_db[zip_code_db$zipcode == "63145", "lng"] <- -90.36077900
+
+  zip_code_db[zip_code_db$zipcode == "63145", "lat"] <- 38.74729100
+  zip_code_db[zip_code_db$zipcode == "63145", "lng"] <- -90.36077900
+
+  zip_code_db[zip_code_db$zipcode == "75261", "lat"] <- 32.89132800
+  zip_code_db[zip_code_db$zipcode == "75261", "lng"] <- -97.03959300
+  ##### end insert #####
+
   # Save the updated zip_code_db file to package data directory
   save(zip_code_db, file = paste0(system.file("data", package = "zipcodeR"), "/zip_code_db.rda"))
 
--- R/update_missing_zip_codes.R
+++ R/update_missing_zip_codes.R
@@ -1,9 +1,24 @@
-load("./data/zip_code_data.rda")
+# load("./data/zip_code_db.rda")
 
-zip_code_db[zip_code_db$zipcode == "20591", "lat"] <- 38.88754600
-zip_code_db[zip_code_db$zipcode == "20591", "lng"] <- -77.02234500
+# zip_code_db[zip_code_db$zipcode == "20591", "lat"] <- 38.88754600
+# zip_code_db[zip_code_db$zipcode == "20591", "lng"] <- -77.02234500
+#
+# zip_code_db[zip_code_db$zipcode == "08405", "lat"] <- 39.35994800
+# zip_code_db[zip_code_db$zipcode == "08405", "lng"] <- -74.43353700
+#
+# zip_code_db[zip_code_db$zipcode == "15231", "lat"] <- 40.49276700
+# zip_code_db[zip_code_db$zipcode == "15231", "lng"] <- -80.24290100
+#
+# zip_code_db[zip_code_db$zipcode == "60666", "lat"] <- 41.97896400
+# zip_code_db[zip_code_db$zipcode == "60666", "lng"] <- -87.90241700
+#
+# zip_code_db[zip_code_db$zipcode == "63145", "lat"] <- 38.74729100
+# zip_code_db[zip_code_db$zipcode == "63145", "lng"] <- -90.36077900
+#
+# zip_code_db[zip_code_db$zipcode == "63145", "lat"] <- 38.74729100
+# zip_code_db[zip_code_db$zipcode == "63145", "lng"] <- -90.36077900
+#
+# zip_code_db[zip_code_db$zipcode == "75261", "lat"] <- 32.89132800
+# zip_code_db[zip_code_db$zipcode == "75261", "lng"] <- -97.03959300
 
-zip_code_db[zip_code_db$zipcode == "08405", "lat"] <- 39.35994800
-zip_code_db[zip_code_db$zipcode == "08405", "lng"] <- -74.43353700
-
-save(zip_code_db, file = "./data/zip_code_data.rda")
+# save(zip_code_db, file = "./data/zip_code_db.rda")
